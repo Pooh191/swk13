8f42acbb1fff526c449a6583c03c419b44211e6a
Pooh191 <robloxgamemer012@gmail.com>
2024-06-03 23:59:58 +0700

    web213

---
+++ scripts.js
@@ -0,0 +1,27 @@
+function updateClock() {
+    var now = new Date();
+    var days = ['อาทิตย์', 'จันทร์', 'อังคาร', 'พุธ', 'พฤหัสบดี', 'ศุกร์', 'เสาร์'];
+    var day = days[now.getDay()];
+
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    var timeString = hours + ':' + minutes + ':' + seconds;
+    var dateString = 'วัน' + day + 'ที่ ';
+
+    var months = ['มกราคม', 'กุมภาพันธ์', 'มีนาคม', 'เมษายน', 'พฤษภาคม', 'มิถุนายน', 'กรกฎาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม', 'พฤศจิกายน', 'ธันวาคม'];
+    var month = months[now.getMonth()];
+    var date = now.getDate();
+    var year = now.getFullYear() + 543;
+
+    var fullDateTimeString = dateString + date + ' ' + month + ' ' + year + ' เวลา ' + timeString;
+    
+    document.getElementById('clock').innerText = fullDateTimeString;
+}
+
+setInterval(updateClock, 1000);
